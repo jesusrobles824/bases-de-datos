02b0464fee7dbdd86dd2ebb707392a7f9b19eb24
jesus <jesusrobles3500@gmail.com>
2025-10-16 09:12:30 -0300

    Update ejecutar_código.py

--- ejecutar_código.py
+++ ejecutar_código.py
@@ -266,6 +266,8 @@ RIN = RIN[['RIN','RIN_pp','RIN_va','RIN_pp_va']]
 RIN = RIN[RIN.index>'2022-12-30']
 RIN = RIN[::-1]
 RIN.columns = ['RIN','RIN a precios 31/01/25','RIN variación acumulada respecto al 30/12/24','RIN variación acumulada respecto al 30/12/25 a precios 31/01/25']
+for col in RIN.columns:
+    RIN[col] = pd.to_numeric(RIN[col], errors='coerce')
 RIN = RIN.round(2)
 RIN.to_csv('Reservas_Netas.csv', index=True)
 
@@ -338,6 +340,7 @@ diar_bas_var.to_csv('Depósitos_tesoro_variación_diaria_y_factores_de_explicaci
 #IED = IED.round(2)
 #IED = IED[::-1]
 #IED.to_csv('Inversión_Extranjera_Directa_Trimestral.csv',index=True)
+
 
 
 
